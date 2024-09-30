261fabe80ee9b7f739951852bdeb967de75e2555
V1taS <SosinVitalii@iCloud.com>
2024-09-30 20:27:31 +0200

    Add local framework Network

---
+++ LocalDependencies/FDNetwork/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "FDNetwork",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "FDNetwork",
+      targets: ["FDNetwork"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "FDNetwork",
+      dependencies: []
+    )
+  ]
+)
