c0f9db263f76e9e4a4659d458cfe3fffb2917ec8
V1taS <SosinVitalii@iCloud.com>
2024-09-30 20:26:38 +0200

    Add local framework Abstractions

---
+++ LocalDependencies/FDAbstractions/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "FDAbstractions",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "FDAbstractions",
+      targets: ["FDAbstractions"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "FDAbstractions",
+      dependencies: []
+    )
+  ]
+)
